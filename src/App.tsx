b3bef4cf1dd2974557cfc64ee79b9b9ee28a2582
shadow <lightshadow416@gmail.com>
2024-03-16 20:35:49 +0100

    project checkpoint {1}

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./style/input.css";
 import "./App.css";
-// import { BrowserRouter as Router , Routes , Route , Link } from react-router-dom
 // components
 import { MyContainer } from "./components";
 function App() {
--- src/components/MyContainer/MyContainer.tsx
+++ src/components/MyContainer/MyContainer.tsx
@@ -1,4 +1,17 @@
 import "./mycontainer.css";
+// router
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+// components
+import { Editor, ColorPicker } from "../../components";
 export default function MyContainer() {
-  return <div className=" bg-mainPages w-full">sb</div>;
+  return (
+    <div className="w-full">
+      hello
+      <BrowserRouter>
+        <Routes>
+          <Route path="editor" element={<Editor />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
